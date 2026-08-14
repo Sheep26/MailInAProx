9b44e2379a9d449ccd1a9260659d4af62b247a99
Sheep26 <drew@drewfitzgerald.co.nz>
2026-08-14 21:45:29 +1200

    Rah

--- email/resend.js
+++ email/resend.js
@@ -33,6 +33,6 @@ export class EmailResend extends Email {
 
     async handle(body) {
         const { data } = await this.resend.emails.receiving.get(body.data.email_id);
-        console.log(data);
+        console.log(data.data.text);
     }
 }
