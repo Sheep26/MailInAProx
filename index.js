a5b27c89404fe9407e36bf30f1647b6cf4abcb6d
Sheep26 <drew@drewfitzgerald.co.nz>
2026-08-14 23:24:35 +1200

    Hopefully the backend to sending

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ app.post('/recieve', (req, res) => {
 });
 
 app.post("/send", async (req, res) => {
+    const session = sessionManager.getSession(req.cookies.session);
 
+    if (!session)
+        return res.sendStatus(401);
+
+    const user = await database.getUserEmail(session.email);
+    await email.send(`${user.username} <${session.user_id}>`, req.body.to, session.user_id, req.body.subject, req.body.content);
 });
 
 app.post('/login', async (req, res) => {
