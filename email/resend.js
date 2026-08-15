57f45615c272ec619372c586ca28aa40eae30603
Sheep26 <drew@drewfitzgerald.co.nz>
2026-08-15 19:09:16 +1200

    Rah

--- email/resend.js
+++ email/resend.js
@@ -1,17 +1,5 @@
 import { Resend } from 'resend';
-import { Email } from './email.js';
-
-function parseEmailAddress(value) {
-    const match = value.match(/^\s*(.*?)\s*<([^<>]+)>\s*$/);
-    let values = {name: null, email: value.trim()};
-
-    if (match) {
-        values.name = match[1].trim();
-        values.email = match[2].trim();
-    }
-
-    return values;
-}
+import { Email, parseEmailAddress } from './email.js';
 
 export class EmailResend extends Email {
     constructor (database, auth) {
