a1a5330ebd6d5571b6b09de0c4249173ffbf8c7e
Sheep26 <drew@drewfitzgerald.co.nz>
2026-08-15 17:16:53 +1200

    rah

--- database/database.js
+++ database/database.js
@@ -10,7 +10,7 @@ export class DatabaseManager {
     }
 
     async addEmail(to, from, reply_to, bcc, cc, mail_id, message_id, html_format, subject, content) {
-        await db.execute('INSERT INTO emails (mail_to, mail_from, reply_to, bcc, cc, mail_id, message_id, html_format, subject, content, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+        await db.execute('INSERT INTO emails (mail_to, mail_from, reply_to, bcc, cc, mail_id, message_id, html_format, subject, content, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
             to,
             from,
             reply_to,
